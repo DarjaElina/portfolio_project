5d85b31f00cb32c4e080e818f103d99a70b67ffc
DarjaElina <daria.elina98@gmail.com>
2025-07-04 18:08:31 +0300

    remove unused event listener

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.querySelectorAll('.toggle-theme').forEach((el)=>{el.addEventListener('c
 hamburger.addEventListener('click',()=>{sidebar.classList.toggle('visible')})
 document.addEventListener('click',(event)=>{if(!sidebar.contains(event.target)&&!hamburger.contains(event.target)){sidebar.classList.remove('visible')}});const theme={dark:!1}
 const setTheme=()=>{const currentTheme=JSON.parse(localStorage.getItem('theme'))||theme;localStorage.setItem('theme',JSON.stringify(currentTheme));if(currentTheme.dark){document.documentElement.classList.add('dark-mode')}}
-setTheme();document.querySelector('#contact-btn').addEventListener('click',()=>{document.querySelector('#contact').scrollIntoView()})
+setTheme();
 const codeLines=["let life = {",'    purpose: "create",','    passion: "coding"',"};","",'console.log(`${life.purpose}','through ${life.passion}.`);'];let lineIndex=0;let charIndex=0;let speed=50;const codeBlock=document.getElementById("code-block");function typeWriter(){if(lineIndex<codeLines.length){let currentLine=codeLines[lineIndex];if(charIndex<currentLine.length){codeBlock.innerHTML+=currentLine.charAt(charIndex);charIndex++;setTimeout(typeWriter,speed)}else{codeBlock.innerHTML+="\n";charIndex=0;lineIndex++;setTimeout(typeWriter,speed*2)}
 Prism.highlightElement(codeBlock)}else{setTimeout(()=>{resetTyping();typeWriter()},2000)}}
 function resetTyping(){codeBlock.innerHTML="";lineIndex=0;charIndex=0}
